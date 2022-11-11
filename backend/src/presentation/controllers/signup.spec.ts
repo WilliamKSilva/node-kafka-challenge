4cc375b30a277e65d7d3683f5b21360f9f381dce
Felipe Borges <wilz.kelvin12@gmail.com>
2022-11-11 15:09:00 -0300

    test: ensure name field is not empty

--- backend/src/presentation/controllers/signup.spec.ts
+++ backend/src/presentation/controllers/signup.spec.ts
@@ -25,4 +25,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.code).toBe(400)
     expect(httpResponse.body).toEqual(new MissingFieldError('email'))
   })
+
+  it('Should check if name field is not empty', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'test@test.com',
+        password: 'test12345'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.code).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingFieldError('name'))
+  })
 })
