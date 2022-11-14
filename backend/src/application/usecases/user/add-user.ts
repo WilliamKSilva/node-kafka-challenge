c93647a34595d148174afb4942a9e1bcc7ad49a0
William <wilz.kelvin12@gmail.com>
2022-11-14 09:03:50 -0300

    feat: ensure encrypter encrypt method is called with the right data

--- backend/src/application/usecases/user/add-user.ts
+++ backend/src/application/usecases/user/add-user.ts
@@ -1,8 +1,19 @@
 import { UserModel } from '../../../domain/models/user'
 import { IAddUserData, IAddUserUseCase } from '../../../domain/usecases/user/add-user'
+import { IEncrypter } from '../../protocols/encrypter'
 
 export class AddUserUseCase implements IAddUserUseCase {
+  private readonly encrypter: IEncrypter
+
+  constructor (encrypter: IEncrypter) {
+    this.encrypter = encrypter
+  }
+
   async add (data: IAddUserData): Promise<UserModel> {
+    const { name, email, password } = data
+
+    const hashed = await this.encrypter.encrypt(password)
+
     return await new Promise(resolve => resolve({
       id: 'id',
       name: 'teste',
