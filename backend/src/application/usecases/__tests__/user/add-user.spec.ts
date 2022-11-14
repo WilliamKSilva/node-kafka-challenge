7f73a3cbfe6481ba4b2741d6a18aee6069e39b91
William <wilz.kelvin12@gmail.com>
2022-11-14 09:04:04 -0300

    test: ensure encrypter encrypt method is called with the right data

--- backend/src/application/usecases/__tests__/user/add-user.spec.ts
+++ backend/src/application/usecases/__tests__/user/add-user.spec.ts
@@ -1,14 +1,36 @@
 import { IAddUserUseCase } from '../../../../domain/usecases/user/add-user'
+import { BcryptAdapter } from '../../../../infra/utils/bcrypt-adapter'
+import { IEncrypter } from '../../../protocols/encrypter'
 import { AddUserUseCase } from '../../user/add-user'
 
 interface IMakeSut {
   sut: IAddUserUseCase
+  encrypter: IEncrypter
 }
 
 const makeSut = (): IMakeSut => {
-  const sut = new AddUserUseCase()
+  const encrypter = new BcryptAdapter()
+  const sut = new AddUserUseCase(encrypter)
 
   return {
-    sut
+    sut,
+    encrypter
   }
 }
+
+describe('AddUserUseCase', () => {
+  it('Should call encrypter with the right data', async () => {
+    const { sut, encrypter } = makeSut()
+
+    const addUserData = {
+      name: 'test',
+      password: 'test12345',
+      email: 'test@test.com'
+    }
+
+    const encrypterSpy = jest.spyOn(encrypter, 'encrypt')
+    await sut.add(addUserData)
+
+    expect(encrypterSpy).toHaveBeenCalledWith('test12345')
+  })
+})
