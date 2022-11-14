2eeee83d310361a58178b71bb0b7634b71e6ed6d
William <wilz.kelvin12@gmail.com>
2022-11-14 08:30:01 -0300

    feat: ensure BcryptAdapter throws if Bcrypt throws

--- backend/src/infra/utils/bcrypt-adapter.ts
+++ backend/src/infra/utils/bcrypt-adapter.ts
@@ -1,9 +1,10 @@
 import { Encrypter } from '../../application/protocols/encrypter'
 import bcrypt from 'bcrypt'
+import { InternalServerError } from '../../presentation/errors/interal-server-error'
 
 export class BcryptAdapter implements Encrypter {
   async encrypt (password: string): Promise<string> {
-    const hashed = bcrypt.hash(password, 10)
+    const hashed = await bcrypt.hash(password, 10)
 
     return await new Promise(resolve => resolve('test'))
   }
