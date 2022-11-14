e0541903ef94cbc00b6d3448fc6a5bfb42221d90
William <wilz.kelvin12@gmail.com>
2022-11-14 11:44:27 -0300

    chore: add mongodb connection and settings

---
+++ backend/src/infra/helpers/mongo-helper.ts
@@ -0,0 +1,22 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  uri: null as string,
+
+  async connect (uri: string) {
+    this.uri = uri
+    this.client = MongoClient.connect(uri)
+  },
+
+  async disconnect () {
+    await this.client.close()
+    this.client = null
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    await this.connect(this.uri)
+
+    return this.client.db().collection(name)
+  }
+}
--- backend/src/infra/mongo/user-repository.ts
+++ backend/src/infra/mongo/user-repository.ts
@@ -1,9 +1,22 @@
 import { IUserRepository } from '../../application/repositories/user-repository'
 import { UserModel } from '../../domain/models/user'
 import { IAddUserData } from '../../domain/usecases/user/add-user'
+import { MongoHelper } from '../helpers/mongo-helper'
 
 export class MongoUserRepository implements IUserRepository {
   async add (data: IAddUserData): Promise<UserModel> {
+    const usersCollection = (await MongoHelper.getCollection('users'))
+    const user: UserModel = {
+      id: '',
+      name: data.name,
+      email: data.email,
+      password: data.password
+    }
 
+    const inserted = await usersCollection.insertOne(user)
+
+    user.id = inserted.insertedId.toJSON()
+
+    return user
   }
 }
