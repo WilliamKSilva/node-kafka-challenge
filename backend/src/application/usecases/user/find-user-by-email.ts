4a122524820fe477c83ee5a10d616f70670d23c3
William <wilz.kelvin12@gmail.com>
2022-11-15 13:28:21 -0300

    feat: ensure FindUserByEmailUseCase calls user repository with the right data

---
+++ backend/src/application/usecases/user/find-user-by-email.ts
@@ -0,0 +1,17 @@
+import { UserModel } from '../../../domain/models/user'
+import { IFindUserByEmailData, IFindUserByEmailUseCase } from '../../../domain/usecases/user/find-user-by-email'
+import { IUserRepository } from '../../repositories/user-repository'
+
+export class FindUserByEmailUseCase implements IFindUserByEmailUseCase {
+  private readonly userRepository: IUserRepository
+
+  constructor (userRepository: IUserRepository) {
+    this.userRepository = userRepository
+  }
+
+  async find (data: IFindUserByEmailData): Promise<UserModel> {
+    const user = this.userRepository.find(data)
+
+    return null
+  }
+}
