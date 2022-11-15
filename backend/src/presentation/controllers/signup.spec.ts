7717d124c49c96b452bffce1bd823c36dba8cb8b
William <wilz.kelvin12@gmail.com>
2022-11-15 18:24:52 -0300

    test: ensure SignUpController returns an error if user email provided already exists

--- backend/src/presentation/controllers/signup.spec.ts
+++ backend/src/presentation/controllers/signup.spec.ts
@@ -3,6 +3,7 @@ import { IAddUserData, IAddUserUseCase } from '../../domain/usecases/user/add-us
 import { IController } from '../protocols/http'
 import { MissingFieldError } from '../errors/missing-field-error'
 import { SignUpController } from './signup'
+import { UserAlreadyExistsError } from '../errors/forbiden-error'
 
 interface IMakeSut {
   sut: IController
@@ -115,6 +116,24 @@ describe('SignUp Controller', () => {
     await sut.handle(httpRequest)
 
     expect(addUserUseCaseSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  it('Should return an error if user already exists', async () => {
+    const { sut, addUserUseCaseStub } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'test@test.com',
+        name: 'test',
+        password: 'test12345'
+      }
+    }
+
+    jest.spyOn(addUserUseCaseStub, 'add').mockResolvedValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.code).toBe(400)
+    expect(httpResponse.body).toEqual(new UserAlreadyExistsError())
   })
 
   it('Should return an User on success', async () => {
