54d0d630547effb537953597e452b990a5d067af
William <wilz.kelvin12@gmail.com>
2022-11-16 09:56:00 -0300

    feat: ensure UserRepository findById method returns an valid user

--- backend/src/infra/mongo/__tests__/user-repository.test.ts
+++ backend/src/infra/mongo/__tests__/user-repository.test.ts
@@ -1,17 +1,17 @@
+import { ObjectId } from 'mongodb'
 import { UserModel } from '../../../domain/models/user'
 import { MongoHelper } from '../../helpers/mongo-helper'
+
+let createdUserId
 
 describe('UserRepository', () => {
   beforeAll(async () => {
     await MongoHelper.connect('mongodb://docker-test:admin@localhost:27020')
   })
 
-  beforeEach(async () => {
+  afterAll(async () => {
     const usersCollection = await MongoHelper.getCollection('accounts')
     await usersCollection.deleteMany({})
-  })
-
-  afterAll(async () => {
     await MongoHelper.disconnect()
   })
 
@@ -26,7 +26,32 @@ describe('UserRepository', () => {
     const usersCollection = await MongoHelper.getCollection('users')
     const inserted = await usersCollection.insertOne(userData)
 
+    createdUserId = inserted.insertedId.toJSON()
+
     const user = Object.assign({}, userData, { id: inserted.insertedId })
+
+    expect(user.id).toBeTruthy()
+    expect(user.name).toBe('test')
+    expect(user.email).toBe('test@test.com')
+    expect(user.password).toBe('test_hash')
+  })
+
+  it('Should find an user on findById method', async () => {
+    const usersCollection = await MongoHelper.getCollection('users')
+    const user: UserModel = {
+      id: '',
+      name: '',
+      password: '',
+      email: ''
+    }
+
+    const _id = new ObjectId(createdUserId)
+
+    const mongoUser = await usersCollection.findOne({
+      _id
+    })
+
+    Object.assign(user, mongoUser, { id: mongoUser._id })
 
     expect(user.id).toBeTruthy()
     expect(user.name).toBe('test')
