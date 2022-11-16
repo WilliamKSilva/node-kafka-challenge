9ad9df2609c9d9937bba17918cdd6f869aa13fe2
William <wilz.kelvin12@gmail.com>
2022-11-16 09:56:10 -0300

    feat: ensure UserRepository findById method returns an valid user

--- backend/src/infra/mongo/user-repository.ts
+++ backend/src/infra/mongo/user-repository.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb'
 import { IUserRepository } from '../../application/repositories/user-repository'
 import { UserModel } from '../../domain/models/user'
 import { IAddUserData } from '../../domain/usecases/user/add-user'
@@ -16,6 +17,24 @@ export class MongoUserRepository implements IUserRepository {
     const inserted = await usersCollection.insertOne(data)
 
     const user = Object.assign({}, userData, { id: inserted.insertedId.toJSON() })
+
+    return user
+  }
+
+  async findById (id: string): Promise<UserModel> {
+    const usersCollection = await MongoHelper.getCollection('users')
+    const user: UserModel = {
+      id: '',
+      name: '',
+      password: '',
+      email: ''
+    }
+
+    const _id = new ObjectId(id)
+
+    const mongoUser = await usersCollection.findOne({ _id })
+
+    Object.assign(user, mongoUser, { id: mongoUser._id })
 
     return user
   }
