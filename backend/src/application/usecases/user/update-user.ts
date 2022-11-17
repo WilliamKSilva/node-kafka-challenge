67612f839dc9a9a36c18888a0c46afa7d7a975aa
William <wilz.kelvin12@gmail.com>
2022-11-17 15:26:58 -0300

    feat: ensure UpdateUserController throws user not found error if user was not found

--- backend/src/application/usecases/user/update-user.ts
+++ backend/src/application/usecases/user/update-user.ts
@@ -1,5 +1,6 @@
 import { UserModel } from '../../../domain/models/user'
 import { IUpdateUserData, IUpdateUserUseCase } from '../../../domain/usecases/user/update-user'
+import { UserNotFoundError } from '../../../presentation/errors/user-not-found-error'
 import { IUserRepository } from '../../repositories/user-repository'
 
 export class UpdateUserUseCase implements IUpdateUserUseCase {
@@ -10,6 +11,12 @@ export class UpdateUserUseCase implements IUpdateUserUseCase {
   }
 
   async update (data: IUpdateUserData, userId: string): Promise<UserModel> {
+    const userExists = await this.userRepository.findById(userId)
+
+    if (!userExists) {
+      throw new UserNotFoundError()
+    }
+
     const user = await this.userRepository.update(data, userId)
 
     return null
