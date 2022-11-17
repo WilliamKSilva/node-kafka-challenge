dbd2086ad3344326f55b3886c137b3f90c2efa55
William <wilz.kelvin12@gmail.com>
2022-11-17 15:27:44 -0300

    test: ensure UpdateUserUseCase throws if user repository findById method throws

--- backend/src/application/usecases/__tests__/user/update-user.spec.ts
+++ backend/src/application/usecases/__tests__/user/update-user.spec.ts
@@ -1,5 +1,6 @@
 
 import { IUpdateUserUseCase } from '../../../../domain/usecases/user/update-user'
+import { UserNotFoundError } from '../../../../presentation/errors/user-not-found-error'
 import { UserRepositoryInMemory } from '../../../repositories/in-memory/user-repository'
 import { IUserRepository } from '../../../repositories/user-repository'
 import { UpdateUserUseCase } from '../../user/update-user'
@@ -20,7 +21,7 @@ const makeSut = (): IMakeSut => {
 }
 
 describe('UpdateUserUseCase', () => {
-  it('Should call sut update method with right data', async () => {
+  it('Should call user repository update method with the right data', async () => {
     const { sut } = makeSut()
 
     const userData = {
@@ -35,5 +36,39 @@ describe('UpdateUserUseCase', () => {
     await sut.update(userData, userId)
 
     expect(sutSpy).toHaveBeenCalledWith(userData, userId)
+  })
+
+  it('Should throws user not found error if user repository returns nothing', async () => {
+    const { sut, userRepository } = makeSut()
+
+    const userData = {
+      name: 'test',
+      email: 'test@test.com',
+      password: 'test12345'
+    }
+
+    const userId = 'id'
+
+    jest.spyOn(userRepository, 'findById').mockResolvedValueOnce(new Promise((resolve, reject) => resolve(null)))
+    const promise = sut.update(userData, userId)
+
+    await expect(promise).rejects.toEqual(new UserNotFoundError())
+  })
+
+  it('Should throws if user repository findById method throws', async () => {
+    const { sut, userRepository } = makeSut()
+
+    const userData = {
+      name: 'test',
+      email: 'test@test.com',
+      password: 'test12345'
+    }
+
+    const userId = 'id'
+
+    jest.spyOn(userRepository, 'findById').mockResolvedValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.update(userData, userId)
+
+    await expect(promise).rejects.toThrow()
   })
 })
