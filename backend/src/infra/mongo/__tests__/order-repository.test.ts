aa666c66c3b4528bf5f5d22b02cd6a4af7695b65
William <wilz.kelvin12@gmail.com>
2022-11-19 20:11:09 -0300

    test: ensure MongoOrderRepository find method returns an order

--- backend/src/infra/mongo/__tests__/order-repository.test.ts
+++ backend/src/infra/mongo/__tests__/order-repository.test.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb'
 import { OrderModel, Status } from '../../../domain/models/order'
 import { MongoHelper } from '../../helpers/mongo-helper'
 
@@ -28,6 +29,26 @@ describe('UserRepository', () => {
     createdOrderId = inserted.insertedId.toJSON()
 
     const order = Object.assign({}, orderData, { id: inserted.insertedId.toJSON() })
+
+    expect(order.id).toBeTruthy()
+    expect(order.name).toBe('test')
+    expect(order.description).toBe('test')
+    expect(order.status).toBe(Status.pending)
+  })
+
+  it('Should find an order', async () => {
+    const ordersCollection = await MongoHelper.getCollection('orders')
+
+    const _id = new ObjectId(createdOrderId)
+
+    const mongoUser = await ordersCollection.findOne({ _id })
+
+    const order: OrderModel = {
+      id: mongoUser._id.toJSON(),
+      name: mongoUser.name,
+      description: mongoUser.description,
+      status: mongoUser.status
+    }
 
     expect(order.id).toBeTruthy()
     expect(order.name).toBe('test')
