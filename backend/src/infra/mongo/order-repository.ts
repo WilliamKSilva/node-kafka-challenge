f645dbb9a6a027bc6161afdf8487ae58308b3e1b
William <wilz.kelvin12@gmail.com>
2022-11-19 22:42:38 -0300

    feat: ensure MongoOrderRepository update methods update and order

--- backend/src/infra/mongo/order-repository.ts
+++ backend/src/infra/mongo/order-repository.ts
@@ -2,6 +2,7 @@ import { ObjectId } from 'mongodb'
 import { IOrderRepository } from '../../application/repositories/order-repository'
 import { OrderModel, Status } from '../../domain/models/order'
 import { IAddOrderData } from '../../domain/usecases/order/add-order'
+import { IUpdateOrderData } from '../../domain/usecases/order/update-order'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class MongoOrderRepository implements IOrderRepository {
@@ -25,6 +26,29 @@ export class MongoOrderRepository implements IOrderRepository {
     const ordersCollection = await MongoHelper.getCollection('orders')
 
     const _id = new ObjectId(orderId)
+
+    const mongoUser = await ordersCollection.findOne({ _id })
+
+    const order: OrderModel = {
+      id: mongoUser._id.toJSON(),
+      name: mongoUser.name,
+      description: mongoUser.description,
+      status: mongoUser.status
+    }
+
+    return order
+  }
+
+  async update (data: IUpdateOrderData, orderId: string): Promise<OrderModel> {
+    const ordersCollection = await MongoHelper.getCollection('orders')
+
+    const _id = new ObjectId(orderId)
+
+    await ordersCollection.updateOne({ _id }, {
+      $set: {
+        ...data
+      }
+    })
 
     const mongoUser = await ordersCollection.findOne({ _id })
 
